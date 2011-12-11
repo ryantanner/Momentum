6a6bab2bfdd056e850db96fd7389436e21dd2d43
akeller <akeller@trinity.edu>
2011-12-10 19:59:35 -0600

    Added Facebook Like, G+

--- war/js/Tweet.js
+++ war/js/Tweet.js
@@ -25,6 +25,12 @@
 	 			}
 			}
 				
+				(function() {
+				    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+				    po.src = 'https://apis.google.com/js/plusone.js';
+				    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+				  })();
+				
 				(function(d, s, id) {
 					  var js, fjs = d.getElementsByTagName(s)[0];
 					  if (d.getElementById(id)) return;
