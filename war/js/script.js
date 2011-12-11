213bc5de0000294059daeb69e9b3d98715132766
Ryan Tanner <rtanner1@trinity.edu>
2011-12-10 19:47:27 -0600

    geolocation

--- war/js/script.js
+++ war/js/script.js
@@ -1,45 +1,46 @@
+function getLatLng(callback) {
+	if (callback)	{
+		if ( navigator.geolocation ) {
+			 navigator.geolocation.getCurrentPosition( 
+	        	function(position) {
+					var pos = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+	            	callback.call(pos);
+				}, 
+	        	function(error) {
+					var pos = new google.maps.LatLng(29.464700,-98.481260);
+					callback(pos);	
+	            }
+	        );      
+	    } else {
+			var pos = new google.maps.LatLng(29.464700,-98.481260);
+			callback.call(pos);	
+	    }
+	}
+}
 
-    $(function() {
+
+    $(document).ready(function() {
 		
 		$('#map_container').hide();
 
-		var center;
 		getLatLng(function(position) {
-			center = position;
+			var center = position;
+			$('#map_canvas').gmap({'center': getLatLng(), 'callback': function () {
+					$('#map_canvas').gmap('addMarker', {'position': center, 'title': 'Hello world!'});
+				}
+			});
+
+			$('#ex1-terrain').toggle(function () {             
+				$('#map_canvas').gmap('option', 'mapTypeId', google.maps.MapTypeId.TERRAIN);
+			}, 
+			function () { 
+				$('#map_canvas').gmap('option', 'mapTypeId', google.maps.MapTypeId.ROADMAP); 
+			} 
+			);
 		});
-		$('#map_canvas').gmap({'center': getLatLng(), 'callback': function () {
-				$('#map_canvas').gmap('addMarker', {'position': center, 'title': 'Hello world!'});
-			}
-		});
-		
-		$('#ex1-terrain').toggle(function () {             
-			$('#map_canvas').gmap('option', 'mapTypeId', google.maps.MapTypeId.TERRAIN);
-		}, 
-		function () { 
-			$('#map_canvas').gmap('option', 'mapTypeId', google.maps.MapTypeId.ROADMAP); 
-		} 
-		);
-		
-		
-		
+
     });
 
-	function getLatLng(callback) {
-		
-		if ( navigator.geolocation ) {
-			 navigator.geolocation.getCurrentPosition ( 
-            	function(position) {
-                	callback.call(new google.maps.LatLng(position.coords.latitude,position.coords.longitude));
- 				}, 
-            	function(error) {
-					callback.call(new google.maps.LatLng(29.464700,-98.481260));	
-                }
-            );      
-        } else {
-					callback.call(new google.maps.LatLng(29.464700,-98.481260));	
-        }
-		
-	}
 
 	function getDirections(start,end)	{
 		// builds DirectionRequest object for google maps api
@@ -56,30 +57,32 @@
 
 	}
 	
-	function geolocate()	{
+	function geolocate(callback)	{
 		// geocoder
-		
-		
-		
-		
+		// returns string representing user location
+		if (callback) {
+			getLatLng(function(position)	{
+				var geocoder = new google.maps.Geocoder();
+				geocoder.geocode({'latLng': position},function(results,status) {
+					if (status == google.maps.GeocoderStatus.OK) {
+				        if (results[1]) {
+				          callback(results[1].formatted_address);
+				        }
+			      } else {
+			        	alert("Geocoder failed due to: " + status);
+						callback("");
+			      }
+				});
+			});
+		}
+		else
+			alert("error");
 	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+		
+	$(document).ready(function($) {
+		
+		$('#geolocater').click(function() {
+			geolocate(function(pos) { $('#from').val(pos); });
+			return false;
+		});
+	});
