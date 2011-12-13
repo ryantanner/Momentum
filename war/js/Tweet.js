4ff4ffea24f02da497b469892e8a0fae542ac235
akeller <akeller@trinity.edu>
2011-12-12 22:34:50 -0600

    Twitter's back

--- war/js/Tweet.js
+++ war/js/Tweet.js
@@ -1,11 +1,5 @@
 
-				(function() {
-				    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-				    po.src = 'https://apis.google.com/js/plusone.js';
-				    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-				  })();
-				
-				(function(d, s, id) {
+					(function(d, s, id) {
 					  var js, fjs = d.getElementsByTagName(s)[0];
 					  if (d.getElementById(id)) return;
 					  js = d.createElement(s); js.id = id;
